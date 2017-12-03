5b73d5b62450a935cf9df1c8d7ab4fbab223d276
TonyCheng <tcheng981@berkeley.edu>
2017-12-02 16:37:52 -0800

    Sign Up Page

--- Custom-App/Custom-App/Authentication/LoginViewController.swift
+++ Custom-App/Custom-App/Authentication/LoginViewController.swift
@@ -21,27 +21,18 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBAction func logInPressed(_ sender: UIButton) {
         guard let emailText = emailTextField.text else { return }
         guard let passwordText = passwordTextField.text else { return }
-//        let emailText = emailTextField.text
-//        let passwordText = passwordTextField.text
-        
+
         if emailText == "" || passwordText == "" {
-            //Alert to tell the user that there was an error because they didn't fill anything in the textfields
             let alertController = UIAlertController(title: "Log In Error", message: "Please enter an email and password.", preferredStyle: .alert)
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(defaultAction)
             self.present(alertController, animated: true, completion: nil)
         } else {
-//            print(emailText)
-//            print(passwordText)
             Auth.auth().signIn(withEmail: emailText, password: passwordText) { (user, error) in
                 if error == nil {
-                    print("ugh rip dude \n")
-//                    self.performSegue(withIdentifier: "LogInToMain", sender: self)
+                    self.performSegue(withIdentifier: "LogInToMain", sender: self)
                 } else {
-                    print("uhhh")
-                    //Tells the user that there is an error and then gets firebase to tell them the error
                     let alertController = UIAlertController(title: "Log In Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    
                     let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
                     alertController.addAction(defaultAction)
                     self.present(alertController, animated: true, completion: nil)
@@ -63,10 +54,9 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        // Checks if user is already signed in and skips login
-        if Auth.auth().currentUser != nil {
-            self.performSegue(withIdentifier: "LogInToMain", sender: self)
-        }
+//        if Auth.auth().currentUser != nil {
+//            self.performSegue(withIdentifier: "LogInToMain", sender: self)
+//        }
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
